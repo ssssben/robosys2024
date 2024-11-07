d0d75f3de4005038c78553e9134ebd6d0b82e4d1
bun <vlongbf@gmail.com>
2024-11-07 12:17:21 +0900

    A

--- test.bash
+++ test.bash
@@ -1,15 +1,21 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2024 bun
 
 ng () {
-	echo ${1}行目が違うよ
+	echo ${1}行目が違う
 	res=1
 }
 
 res=0
-
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+[ "$(out)" = 15 ] || ng "$LINENO"
 
-[ "${ras}" = 0 ] && echo OK
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "$(out)" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "$(out)" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
